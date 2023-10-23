711ac52e0c2d4b0f323445dc78aa795eb87a3709
nakamurakko <28667444+nakamurakko@users.noreply.github.com>
2023-10-23 09:43:03 +0900

    コメント追加。

--- WpfTextReader/MainWindowViewModel.cs
+++ WpfTextReader/MainWindowViewModel.cs
@@ -43,12 +43,14 @@ internal sealed partial class MainWindowViewModel : ObservableObject
     {
         using (SpeechSynthesizer synthesizer = new SpeechSynthesizer())
         {
+            // 利用可能な音声を一覧に追加する。
             foreach (InstalledVoice voice in synthesizer.GetInstalledVoices())
             {
                 this.Voices.Add(voice);
             }
         }
 
+        // 音声の1つ目を選択状態にする。
         this.SelectedVoice = this.Voices.FirstOrDefault();
     }
 
@@ -75,7 +77,7 @@ internal sealed partial class MainWindowViewModel : ObservableObject
         // 「SpeechSynthesizer への最後の参照を解放する前に、必ず Dispose を呼び出してください。 そうしないと、ガベージ コレクターが SpeechSynthesizer オブジェクトの Finalize メソッドを呼び出すまで、使用されているリソースは解放されません。」
         // とあるので、メソッド内で SpeechSynthesizer をインスタンス化する場合は
         // SpeechSynthesizer.SpeakAsync() は使用しない方が良い。
-        // <https://learn.microsoft.com/ja-jp/dotnet/api/system.speech.synthesis.speechsynthesizer?view=netframework-4.8.1&viewFallbackFrom=net-6.0#:~:text=SpeechSynthesizer%E3%81%B8%E3%81%AE%E6%9C%80%E5%BE%8C%E3%81%AE%E5%8F%82%E7%85%A7%E3%82%92%E8%A7%A3%E6%94%BE%E3%81%99%E3%82%8B%E5%89%8D%E3%81%AB%E3%80%81%E5%BF%85%E3%81%9ADispose%E3%82%92%E5%91%BC%E3%81%B3%E5%87%BA%E3%81%97%E3%81%A6%E3%81%8F%E3%81%A0%E3%81%95%E3%81%84%E3%80%82%20%E3%81%9D%E3%81%86%E3%81%97%E3%81%AA%E3%81%84%E3%81%A8%E3%80%81%E3%82%AC%E3%83%99%E3%83%BC%E3%82%B8%20%E3%82%B3%E3%83%AC%E3%82%AF%E3%82%BF%E3%83%BC%E3%81%8C%20SpeechSynthesizer%20%E3%82%AA%E3%83%96%E3%82%B8%E3%82%A7%E3%82%AF%E3%83%88%E3%81%AE%20Finalize%20%E3%83%A1%E3%82%BD%E3%83%83%E3%83%89%E3%82%92%E5%91%BC%E3%81%B3%E5%87%BA%E3%81%99%E3%81%BE%E3%81%A7%E3%80%81%E4%BD%BF%E7%94%A8%E3%81%95%E3%82%8C%E3%81%A6%E3%81%84%E3%82%8B%E3%83%AA%E3%82%BD%E3%83%BC%E3%82%B9%E3%81%AF%E8%A7%A3%E6%94%BE%E3%81%95%E3%82%8C%E3%81%BE%E3%81%9B%E3%82%93%E3%80%82>
+        // <https://learn.microsoft.com/ja-jp/dotnet/api/system.speech.synthesis.speechsynthesizer#:~:text=SpeechSynthesizer%E3%81%B8%E3%81%AE%E6%9C%80%E5%BE%8C%E3%81%AE%E5%8F%82%E7%85%A7%E3%82%92%E8%A7%A3%E6%94%BE%E3%81%99%E3%82%8B%E5%89%8D%E3%81%AB%E3%80%81%E5%BF%85%E3%81%9ADispose%E3%82%92%E5%91%BC%E3%81%B3%E5%87%BA%E3%81%97%E3%81%A6%E3%81%8F%E3%81%A0%E3%81%95%E3%81%84%E3%80%82%20%E3%81%9D%E3%81%86%E3%81%97%E3%81%AA%E3%81%84%E3%81%A8%E3%80%81%E3%82%AC%E3%83%99%E3%83%BC%E3%82%B8%20%E3%82%B3%E3%83%AC%E3%82%AF%E3%82%BF%E3%83%BC%E3%81%8C%20SpeechSynthesizer%20%E3%82%AA%E3%83%96%E3%82%B8%E3%82%A7%E3%82%AF%E3%83%88%E3%81%AE%20Finalize%20%E3%83%A1%E3%82%BD%E3%83%83%E3%83%89%E3%82%92%E5%91%BC%E3%81%B3%E5%87%BA%E3%81%99%E3%81%BE%E3%81%A7%E3%80%81%E4%BD%BF%E7%94%A8%E3%81%95%E3%82%8C%E3%81%A6%E3%81%84%E3%82%8B%E3%83%AA%E3%82%BD%E3%83%BC%E3%82%B9%E3%81%AF%E8%A7%A3%E6%94%BE%E3%81%95%E3%82%8C%E3%81%BE%E3%81%9B%E3%82%93%E3%80%82>
         //SpeechSynthesizer synthesizer = new SpeechSynthesizer();
         //synthesizer.SelectVoice(this.SelectedVoice.VoiceInfo.Name);
         //synthesizer.SpeakAsync(this.TargetText);
@@ -85,7 +87,9 @@ internal sealed partial class MainWindowViewModel : ObservableObject
         {
             using (SpeechSynthesizer synthesizer = new SpeechSynthesizer())
             {
+                // 選択音声を設定。
                 synthesizer.SelectVoice(this.SelectedVoice.VoiceInfo.Name);
+                // テキストを読み上げる。
                 synthesizer.Speak(this.TargetText);
             }
         });
